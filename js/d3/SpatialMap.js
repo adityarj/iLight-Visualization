7b6ba15419a74d75dc5c49856f73739e0fa625e6
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-04-30 16:58:43 +0800

    added leaflet map?

--- js/d3/SpatialMap.js
+++ js/d3/SpatialMap.js
@@ -8,12 +8,23 @@ d3.csv("data/photoData.csv",function (error, data) {
             num: d["Photo num"],
             group: d["Group type"],
             exhibit: d.exhibition,
-            time: moment(d.TIMESTAMP),
+            time: moment("25 April, 2017"),
             avg_color: d.avg_color
         })
     });
 
     processedData.forEach(function (d, i) {
-        console.log(d.time)
-    })
+    });
+
+    var MyMap = L.map('spatialMap',{zoomControl: false}).setView([1.2853822889881301, 103.85621971799992], 16);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'adityarj.0mbl5ab1',
+        accessToken: 'pk.eyJ1IjoiYWRpdHlhcmoiLCJhIjoiY2lwczQ0dzFmMDJqcWZsbTI3bDJld2JoNSJ9.mpkJWXaUYoE1jtAn6a9Mvw'
+    }).addTo(MyMap);
+
+    console.log("hi");
+
+
 });
