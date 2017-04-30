b7d89bff3e63f92507436d7a643dc6f93e079928
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2017-04-30 17:33:38 +0800

    added data for mapping

--- js/d3/SpatialMap.js
+++ js/d3/SpatialMap.js
@@ -1,6 +1,125 @@
 /**
  * Created by HP-PC on 30-04-2017.
  */
+
+var locationIndex = [
+    {
+        exhibit: "artzoo",
+        coordinates: [1.2879965395090667,103.85899351764238]
+    },
+    {
+        exhibit: "ataraxia",
+        coordinates: [1.2801888833519546,103.85577294684481]
+    },
+    {
+        exhibit: "artbox",
+        coordinates: [1.2805838010231536,103.85727882385254]
+    },
+    {
+        exhibit: "colourful garden of lights",
+        coordinates: [1.2843828078497908,103.85348770345445]
+    },
+    {
+        exhibit: "dande-lier",
+        coordinates: [1.2889414128390326,103.85709259237046]
+
+    },
+    {
+        exhibit: "eco.me",
+        coordinates: [1.2816457,103.85406020000005]
+    },
+    {
+        exhibit: "forever young!",
+        coordinates: [1.2806930126911908,103.85490390981431]
+    },
+    {
+        exhibit: "gastrobeats",
+        coordinates: [1.2804141326208152,103.85746810163255]
+    },
+    {
+        exhibit: "home",
+        coordinates: [1.2803819541491932,103.85625574315782]
+    },
+    {
+        exhibit: "horizontal interference",
+        coordinates: [1.280832452715005,103.85638448919053]
+    },
+    {
+        exhibit: "HYBYCOZO",
+        coordinates: [1.2889521392876848,103.8569960341556]
+    },
+    {
+        exhibit: "i light you so much",
+        coordinates: [1.2816466653522476,103.85464297462022]
+    },
+    {
+        exhibit: "kaldeioscope monolith",
+        coordinates: [1.2808743822627933,103.85462688136613]
+    },
+    {
+        exhibit: "moonflower",
+        coordinates: [1.281132785048215,103.85430309630465]
+    },
+    {
+        exhibit: "northern lights",
+        coordinates: [1.2804892157196675,103.85539743758272]
+    },
+    {
+        exhibit: "ocean pavilion",
+        coordinates: [1.2820016033853014,103.85457131720614]
+    },
+    {
+        exhibit: "passage of inner reflection",
+        coordinates: [1.289059400498349,103.85723206854891]
+    },
+    {
+        exhibit: "secret galaxies",
+        coordinates: [1.286279383748174,103.85926365852356]
+    },
+    {
+        exhibit: "silent disco asia",
+        coordinates: []
+    },
+    {
+        exhibit: "social sparkles",
+        coordinates: [1.2813580342750288,103.85679218627047]
+    },
+    {
+        exhibit: "the body of sea",
+        coordinates: [1.2867942380580968,103.85450005531311]
+    },
+    {
+        exhibit: "the urchin",
+        coordinates: [1.2813580342750288,103.85670635558199]
+    },
+    {
+        exhibit: "ultra light network",
+        coordinates: [1.2821088650516375,103.85427090979647]
+    },
+    {
+        exhibit: "uncle ringo",
+        coordinates: [1.2803068710471985,103.85727498389315]
+    },
+    {
+        exhibit: "very glowing exhibition - very wishing river",
+        coordinates: [1.2805106680332254,103.85475370741915]
+    },
+    {
+        exhibit: "waterfront bazaar",
+        coordinates: [1.280607203441997,103.85770413733553]
+    },
+    {
+        exhibit: "waves",
+        coordinates: [1.2889092348021545,103.85767195082735]
+    },
+    {
+        exhibit: "you lookin' at me?",
+        coordinates: [1.2821195909561476,103.85300490714144]
+    }
+
+];
+
+
 d3.csv("data/photoData.csv",function (error, data) {
     var processedData = [];
     data.forEach(function (d, i) {
